a3751e7760f9a65dd9af5dca8de3563d507b0871
Nguyễn Hoàng Khải <153884234+hoangkhai2503@users.noreply.github.com>
2024-02-15 16:28:21 +0700

    created studentcontroller

---
+++ src/main/java/com/manager/controllers/admin/StudentController.java
@@ -0,0 +1,11 @@
+package com.manager.controllers.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping({ "admin" })
+public class StudentController {
+	
+}
